7f49abf9287ea5636302c581aa92bde414ba4248
D-H-O-R-A <bicontoken@gmail.com>
2024-04-29 19:45:39 -0300

    update

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '6484300463:AAH-qjDrBqLXCpdg0lqSdQ4VoOyXwJPcslI'; // Replace with your own bot token
 const bot = new TelegramBot(token, { polling: true });
-const OpenAI = require("openai");
-
-const openai = new OpenAI({
-  apiKey: "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",
-});
+const fetch = require("node-fetch")
 
 
 bot.on('message',async (msg) => {
@@ -22,7 +18,7 @@ bot.on('message',async (msg) => {
         bot.sendMessage(chatId, "Generating image, please wait...")
         var g = await generateImage(messageText.replaceAll("/image",""))
         // Envia a imagem como um arquivo diretamente do buffer
-        return bot.sendPhoto(chatId, g.url);
+        return bot.sendPhoto(chatId, g);
     }else{
         var g = await generateText(messageText);
         if(g.toLowerCase().includes("openai")){
@@ -36,12 +32,12 @@ bot.on('message',async (msg) => {
 async function generateText(prompt) {
     console.log("Prompt: "+prompt)
     try {
-      const response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo", // Replace with the model you intend to use; check OpenAI documentation for the latest models
-        messages: [{ role: "system", content: prompt }],
-      });
-      console.log(response.choices);
-      return response.choices[0].message.content
+      var response = await fetch(`https://us-central1-bracefaucet.cloudfunctions.net/generateText?input=${prompt}`)
+      if (!response.ok) {
+        return {revised_prompt:"Error creating image.",  url:"Please try again."}
+      }
+      response = await response.json(); // use .json()
+      return response.data
     } catch (error) {
       console.error(error);
       return 'Please try again.';
@@ -51,9 +47,12 @@ async function generateText(prompt) {
 async function generateImage(prompt){
     console.log("Prompt image: "+ prompt)
     try{
-        const image = await openai.images.generate({ model: "dall-e-3", prompt: prompt });
-        console.log(image.data);
-        return image.data[0];
+      var response = await fetch(`https://us-central1-bracefaucet.cloudfunctions.net/generateImage?input=${prompt}`)
+      if (!response.ok) {
+        return {revised_prompt:"Error creating image.",  url:"Please try again."}
+      }
+      response = await response.json(); // use .json()
+      return response.data
     } catch (error){
         return {revised_prompt:"Error creating image.",  url:"Please try again."}
     }
